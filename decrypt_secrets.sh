569ffb296ab6ed8bb02adb7617230a233c716819
Xymos <xymos0@gmail.com>
2020-03-22 00:31:10 +0000

    cleanup

--- decrypt_secrets.sh
+++ decrypt_secrets.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-gpg --quiet --batch --yes --decrypt --passphrase="tFeozZt8okDTajTLNoSmo+0xPUqUBAuesl/vGFLKFTw=" \
+gpg --quiet --batch --yes --decrypt --passphrase=$KEY_STORE_PASSPHRASE \
 --output keystore.properties keystore.properties.gpg
 
-gpg --quiet --batch --yes --decrypt --passphrase="hDiAbX4N8YR9Uzt1gEq66MIJLZsayA5SAjv31ME9oKQ=" \
+gpg --quiet --batch --yes --decrypt --passphrase=$APP_SIGN_KEY_FILE_PASSPHRASE \
 --output ./app/app-sign-key.jks ./app/app-sign-key.jks.gpg
 
