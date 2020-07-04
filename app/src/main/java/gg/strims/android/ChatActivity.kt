e855b55bda016f308c53116b4bf92037fc019bc1
Jack <JackAtGitHub@gmail.com>
2020-07-04 19:11:45 +0100

    Move StrimsClient.kt into separate package.

--- app/src/main/java/gg/strims/android/ChatActivity.kt
+++ app/src/main/java/gg/strims/android/ChatActivity.kt
@@ -32,6 +32,7 @@ import com.google.gson.Gson
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
 import com.xwray.groupie.Item
+import gg.strims.android.clients.StrimsClient
 import gg.strims.android.customspans.CenteredImageSpan
 import gg.strims.android.customspans.NoUnderlineClickableSpan
 import gg.strims.android.models.*
@@ -1848,53 +1849,6 @@ class ChatActivity : AppCompatActivity() {
                 }
             }
             return null
-        }
-    }
-
-    inner class StrimsClient {
-
-        private val client = HttpClient {
-            install(WebSockets)
-        }
-
-        suspend fun onConnect() = client.wss(
-            host = "strims.gg",
-            path = "/ws"
-        ) {
-            while (true) {
-                when (val frame = incoming.receive()) {
-                    is Frame.Text -> {
-                        println(frame.readText())
-                        parseStream(frame.readText())
-                    }
-                    is Frame.Binary -> println(frame.readBytes())
-                }
-            }
-        }
-
-        private fun parseStream(input: String) {
-            val input2 = input.substringAfter("[\"").substringBefore("\"")
-            if (input2 == "STREAMS_SET") {
-                val msg = input.substringAfter("\",").substringBeforeLast(']')
-                val streams: List<Stream>? = Klaxon().parseArray(msg)
-                CurrentUser.streams = streams?.toMutableList()
-            } else if (input2 == "RUSTLERS_SET") {
-                val id = input.substringAfter("\"RUSTLERS_SET\",").substringBefore(",").toLong()
-                if (CurrentUser.streams != null) {
-                    CurrentUser.streams!!.forEach {
-                        if (it.id == id) {
-                            val newRustlers =
-                                input.substringAfter("$id,").substringBefore(",").toInt()
-                            val newAfk =
-                                input.substringAfter("$id,$newRustlers,").substringBefore("]")
-                                    .toInt()
-                            it.rustlers = newRustlers
-                            it.afk_rustlers = newAfk
-                            return
-                        }
-                    }
-                }
-            }
         }
     }
 
